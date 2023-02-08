1f416a58c18e4124b2935e7be2eaf57312ef55c8
Viktor Niconovich <niconovich@gmail.com>
2023-02-08 11:16:56 +0300

    fix footer new

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,21 +1,18 @@
-import {Breadcrumbs, Link,Typography} from "@mui/material";
+import {Breadcrumbs, Link, Typography} from "@mui/material";
 
-import { Button } from '../Button/Button'
-import { FormSignIn } from '../Forms/FormSignIn/FormSignIn'
-import { PageWrapper } from '../PageWrapper/PageWrapper'
+import {Button} from '../Button/Button'
+import {FormSignIn} from '../Forms/FormSignIn/FormSignIn'
+import {PageWrapper} from '../PageWrapper/PageWrapper'
 
 
-export const SignIn = ({ onClickHome }: any) => {
+export const SignIn = ({onClickHome}: any) => {
     return (
         <>
             <PageWrapper title={'Sign In'}
-                // button={
-                //     <Button className='btn' onClick={onClickHome}>{'Back to home'}</Button>
-                // }
-                breadcrumb={<Breadcrumbs>
-                        <Link underline="hover" color="inherit" href='/'>Home</Link>
-                        <Typography color="text.primary">Sign In</Typography>
-                    </Breadcrumbs>  }>
+                         breadcrumb={<Breadcrumbs>
+                             <Link underline="hover" color="inherit" href='/'>Home</Link>
+                             <Typography color="text.primary">Sign In</Typography>
+                         </Breadcrumbs>}>
                 <FormSignIn/>
             </PageWrapper>
         </>
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,20 +1,17 @@
- import {Breadcrumbs, Link,Typography} from "@mui/material";
-import { Button } from '../Button/Button'
-import { FormSignUp } from '../Forms/FormSignUp/FormSignUp'
-import { PageWrapper } from '../PageWrapper/PageWrapper'
+import {Breadcrumbs, Link, Typography} from "@mui/material";
+import {Button} from '../Button/Button'
+import {FormSignUp} from '../Forms/FormSignUp/FormSignUp'
+import {PageWrapper} from '../PageWrapper/PageWrapper'
 
 
-export const SignUp = ({ onClickHome }: any) => {
+export const SignUp = ({onClickHome}: any) => {
     return (
         <>
             <PageWrapper title={'Sign Up'}
-                // button={
-                //     <Button className='btn' onClick={onClickHome}>{'Back to home'}</Button>
-                // }
-                breadcrumb={<Breadcrumbs>
-                    <Link underline="hover" color="inherit" href='/'>Home</Link>
-                    <Typography color="text.primary">Sign Un</Typography>
-                </Breadcrumbs>}>
+                         breadcrumb={<Breadcrumbs>
+                             <Link underline="hover" color="inherit" href='/'>Home</Link>
+                             <Typography color="text.primary">Sign Un</Typography>
+                         </Breadcrumbs>}>
                 <FormSignUp/>
             </PageWrapper>
         </>
